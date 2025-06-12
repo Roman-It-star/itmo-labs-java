da15ba3fd3017ae12519510687d21be58f5a14e3
Roman-It-star <romanop2014@gmail.com>
2025-06-12 21:38:42 +0300

    Labs1

---
+++ src/labs/java/lab1/task2/Main.java
@@ -0,0 +1,11 @@
+package labs.java.lab1.task2;
+
+public class Main {
+    public static void main(String[] args){
+        Double result1 = (46 + 10) * ((double)10/3);
+        Double result2 = (double)(29) * (4) * (-15);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
